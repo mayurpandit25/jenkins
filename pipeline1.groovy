667e60e8574957379cb871cbdbdf7457ab4f04ec
mayur pandit <panditmayur888@gmail.com>
2025-11-11 18:37:31 +0530

    pipeline

--- pipeline1.groovy
+++ pipeline1.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage ('checkout') {
             steps {
-                git 'https://github.com/Pritam-Khergade/student-ui.git'  ------------#url of studentapp.ui
+                git 'https://github.com/Pritam-Khergade/student-ui.git'  
                 echo 'pull successful'
             }
         }
